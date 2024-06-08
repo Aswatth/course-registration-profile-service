a97123fd34e1dfe8ddaf8c7f82b3c10e63af9030
unknown <aswatth56@gmail.com>
2024-06-08 13:42:14 +0530

    Added functionality to update student password

--- controllers/student_profile_controller.go
+++ controllers/student_profile_controller.go
@@ -61,10 +61,36 @@ func (obj *StudentProfileController) UpdateStudentProfile(context *gin.Context) 
 	context.JSON(http.StatusOK, gin.H{"message": "Successfully updated!"})
 }
 
+func (obj *StudentProfileController) UpdateStudentPassword(context *gin.Context) {
+
+	email_id := context.Query("email_id")
+
+	//Fetch from DB
+	fetched_student_profile := obj.service.FetchStudentProfile(email_id)
+
+	type NewPasswordData struct {
+		New_password string
+	}
+
+	var new_password_data NewPasswordData
+
+	//Check if given JSON is valid
+	if err := context.ShouldBindJSON(&new_password_data); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+
+	fetched_student_profile.Password = new_password_data.New_password
+
+	obj.service.UpdateStudentProfile(fetched_student_profile)
+
+	context.JSON(http.StatusOK, gin.H{"message": "Successfully updated password!"})
+}
+
 func (obj *StudentProfileController) RegisterRoutes(rg *gin.RouterGroup) {
 	student_profile_routes := rg.Group("/student")
 
 	student_profile_routes.POST("/create", obj.CreateProfile)
 	student_profile_routes.GET("/fetch", obj.FetchStudentProfile)
 	student_profile_routes.PUT("/update", obj.UpdateStudentProfile)
+	student_profile_routes.PUT("/update_password", obj.UpdateStudentPassword)
 }
