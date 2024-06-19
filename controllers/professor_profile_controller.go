519898e32913359bcfea831f079b9f9e604c4bb3
unknown <aswatth56@gmail.com>
2024-06-19 20:50:48 +0530

    Changes to professor profile controller

--- controllers/professor_profile_controller.go
+++ controllers/professor_profile_controller.go
@@ -19,32 +19,33 @@ func (obj *ProfessorProfileController) FetchProfessorProfile(context *gin.Contex
 	email_id := context.Query("email_id")
 
 	//Fetch from DB
-	fetched_professor_profile := obj.service.FetchProfessorProfile(email_id)
+	fetched_professor_profile, err := obj.service.FetchProfessorProfile(email_id)
 
-	context.JSON(http.StatusOK, fetched_professor_profile)
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
+	} else {
+		context.JSON(http.StatusOK, fetched_professor_profile)
+	}
+
 }
 
 func (obj *ProfessorProfileController) UpdatePassword(context *gin.Context) {
 
 	email_id := context.Param("email_id")
 
-	type new_password struct {
-		New_password string
+	new_password := make(map[string]string)
+
+	if err := context.ShouldBindJSON(&new_password); err != nil {
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"response": err.Error()})
+	} else {
+		err := obj.service.UpdatePassword(email_id, new_password["new_password"])
+
+		if err != nil {
+			context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"response": err.Error()})
+		} else {
+			context.Status(http.StatusOK)
+		}
 	}
-
-	var new_password_data new_password
-
-	if err := context.ShouldBindJSON(&new_password_data); err != nil {
-		context.AbortWithError(http.StatusBadRequest, err)
-	}
-
-	err := obj.service.UpdatePassword(email_id, new_password_data.New_password)
-
-	if err != nil {
-		context.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
-	}
-
-	context.Status(http.StatusOK)
 }
 
 func (obj *ProfessorProfileController) RegisterRoutes(rg *gin.RouterGroup) {
